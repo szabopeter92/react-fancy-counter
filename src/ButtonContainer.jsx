063f6db08ac671f8965b48df270e98df9225675d
Szabó Péter <szabo.p992@gmail.com>
2023-12-21 10:22:04 +0100

    Done

--- src/ButtonContainer.jsx
+++ src/ButtonContainer.jsx
@@ -1,11 +1,7 @@
-import CountButton from "./CountButton";
-
-
-export default function ButtonContainer({setCount, locked}) {
+export default function ButtonContainer({children}) {
   return (
     <div className="button-container">
-      <CountButton type="minus" setCount={setCount} locked={locked} />
-      <CountButton type="plus" setCount={setCount} locked={locked}/>
+      {children}
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,6 +3,7 @@ import ResetButton from "./ResetButton";
 import Count from "./Count";
 import Title from "./Title";
 import ButtonContainer from "./ButtonContainer";
+import CountButton from "./CountButton";
 
 export default function Card() {
   const [count, setCount] = useState(0);
@@ -32,7 +33,10 @@ export default function Card() {
         <Title locked={locked} />
         <Count count={count} />
         <ResetButton setCount={setCount} />
-        <ButtonContainer setCount={setCount} locked={locked} />
+        <ButtonContainer>
+          <CountButton type="minus" setCount={setCount} locked={locked} />
+          <CountButton type="plus" setCount={setCount} locked={locked}/>
+        </ButtonContainer>
     </div>
   )
 }
